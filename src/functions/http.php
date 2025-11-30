1224e3a3849f4c69668bf302ed39869c1495ec38
celionatti <amisuusman@gmail.com>
2025-12-01 00:24:16 +0100

    feat: introduce global HTTP client and batch request helper functions.

--- src/functions/http.php
+++ src/functions/http.php
@@ -2,20 +2,20 @@
 
 declare(strict_types=1);
 
-use Plugs\Http\HTTP;
 use Plugs\Http\Batch;
+use Plugs\Http\HTTPClient;
 use Plugs\Http\HTTPResponse;
 use Plugs\Http\BatchResponse;
 
 if (!function_exists('http')) {
-    function http(?string $baseUri = null): HTTP
+    function http(?string $baseUri = null): HTTPClient
     {
-        $http = HTTP::make();
-        
+        $http = HTTPClient::make();
+
         if ($baseUri !== null) {
             $http->baseUri($baseUri);
         }
-        
+
         return $http;
     }
 }
@@ -23,35 +23,35 @@ if (!function_exists('http')) {
 if (!function_exists('http_get')) {
     function http_get(string $url, array $query = []): HTTPResponse
     {
-        return HTTP::make()->get($url, $query);
+        return HTTPClient::make()->get($url, $query);
     }
 }
 
 if (!function_exists('http_post')) {
     function http_post(string $url, array $data = []): HTTPResponse
     {
-        return HTTP::make()->post($url, $data);
+        return HTTPClient::make()->post($url, $data);
     }
 }
 
 if (!function_exists('http_put')) {
     function http_put(string $url, array $data = []): HTTPResponse
     {
-        return HTTP::make()->put($url, $data);
+        return HTTPClient::make()->put($url, $data);
     }
 }
 
 if (!function_exists('http_patch')) {
     function http_patch(string $url, array $data = []): HTTPResponse
     {
-        return HTTP::make()->patch($url, $data);
+        return HTTPClient::make()->patch($url, $data);
     }
 }
 
 if (!function_exists('http_delete')) {
     function http_delete(string $url, array $data = []): HTTPResponse
     {
-        return HTTP::make()->delete($url, $data);
+        return HTTPClient::make()->delete($url, $data);
     }
 }
 
@@ -66,14 +66,14 @@ if (!function_exists('http_async')) {
     function http_async(array $requests): BatchResponse
     {
         $batch = Batch::make();
-        
+
         foreach ($requests as $key => $request) {
             $method = strtolower($request['method'] ?? 'get');
             $url = $request['url'];
             $data = $request['data'] ?? [];
             $options = $request['options'] ?? [];
             $requestKey = is_string($key) ? $key : null;
-            
+
             switch ($method) {
                 case 'get':
                     $batch->get($url, $options, $requestKey);
@@ -94,7 +94,7 @@ if (!function_exists('http_async')) {
                     $batch->add($method, $url, $options, $requestKey);
             }
         }
-        
+
         return $batch->send();
     }
 }
